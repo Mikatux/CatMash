043bd463a7fc8e3f82107e5dae59cb779972f05c
Mickael MAYEUR <mickael@mayeur.eu>
2016-12-03 18:17:10 +0100

    Add firebase connection

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mika on 12/3/16.
+ */
+const config = {
+  apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+module.exports = config;
